27ff785d18e31addccc49b8d8f253036ccce7bcd
cyril <cyril.souti@gmail.com>
2016-07-24 15:10:03 +0200

    xGetEmail

--- x-gwt-firebase/src/main/java/com/github/spirylics/xgwt/firebase/auth/User.java
+++ x-gwt-firebase/src/main/java/com/github/spirylics/xgwt/firebase/auth/User.java
@@ -1,12 +1,14 @@
 package com.github.spirylics.xgwt.firebase.auth;
 
+import com.google.common.base.Strings;
+import jsinterop.annotations.JsOverlay;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
 @SuppressWarnings("ALL")
 @JsType(isNative = true, namespace = "firebase", name = "User")
 public class User extends UserInfo {
-    
+
     @JsProperty
     public native boolean getEmailVerified();
 
@@ -15,5 +17,14 @@ public class User extends UserInfo {
 
     @JsProperty
     public native UserInfo[] getProviderData();
+
+    @JsOverlay
+    public final String xGetEmail() {
+        if (Strings.isNullOrEmpty(getEmail()) && getProviderData().length > 0) {
+            return getProviderData()[0].getEmail();
+        } else {
+            return getEmail();
+        }
+    }
 
 }
