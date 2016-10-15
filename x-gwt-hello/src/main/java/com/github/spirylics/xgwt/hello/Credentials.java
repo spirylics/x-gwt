a1ce8c0f79084251553704181bca10afb90d69ce
cyril <cyril.souti@gmail.com>
2016-10-15 16:45:03 +0200

    up hello

--- x-gwt-hello/src/main/java/com/github/spirylics/xgwt/hello/Credentials.java
+++ x-gwt-hello/src/main/java/com/github/spirylics/xgwt/hello/Credentials.java
@@ -1,17 +1,17 @@
 package com.github.spirylics.xgwt.hello;
 
 
+import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsProperty;
 import jsinterop.annotations.JsType;
 
-@SuppressWarnings("ALL")
-@JsType(isNative = true, name = "Credentials")
+@JsType(isNative = true, name = "Object", namespace = JsPackage.GLOBAL)
 public class Credentials {
 
     @JsProperty
     public native String getFacebook();
 
     @JsProperty
-    public native void  setFacebook(String facebook);
+    public native void setFacebook(String facebook);
 
 }
--- x-gwt-hello/src/main/java/com/github/spirylics/xgwt/hello/Hello.java
+++ x-gwt-hello/src/main/java/com/github/spirylics/xgwt/hello/Hello.java
@@ -6,8 +6,6 @@ import jsinterop.annotations.JsMethod;
 import jsinterop.annotations.JsPackage;
 import jsinterop.annotations.JsType;
 
-import java.util.Map;
-
 @SuppressWarnings("ALL")
 @JsType(isNative = true, name = "hello")
 public class Hello {
@@ -18,7 +16,7 @@ public class Hello {
     @JsMethod(namespace = JsPackage.GLOBAL)
     public native static Hello hello(String network);
 
-    public native Hello init(Map<String, String> credential, Options options);
+    public native Hello init(Credentials credentials, Options options);
 
     public native Promise<Auth, Error> login();
 
